f3bee738c55d7c19b5695468fc7097e0fe838ddf
Kelsey Low <kklowlow@gmail.com>
2019-02-28 05:07:08 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
-    if artist = Artist.find_by_name(input) # what exactly is this doing?
+    if artist = Artist.find_by_name(input) # if an instance of the artist input by the user is found
       artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |song, i|
         puts "#{i+1}. #{song.name} - #{song.genre.name}"
       end
